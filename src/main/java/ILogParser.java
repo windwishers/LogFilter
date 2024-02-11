fb2021cc1336f9bec9bac9bb48807d3d355d669b
shim seok bo <shadowog@gmail.com>
2024-02-11 12:48:12 +0900

    미사용 코드 삭제

--- src/main/java/ILogParser.java
+++ src/main/java/ILogParser.java
@@ -1,18 +1,7 @@
 import java.awt.Color;
 
-/**
- * 
- */
-
-
-/**
- * 
- */
 public interface ILogParser
 {
-    public static final int TYPE_ANDROID_DDMS   = 0;
-    public static final int TYPE_ANDROID_LOGCAT = 1;
-    
     public LogInfo parseLog(String strText);
     public Color   getColor(LogInfo logInfo);
     public int     getLogLV(LogInfo logInfo);
